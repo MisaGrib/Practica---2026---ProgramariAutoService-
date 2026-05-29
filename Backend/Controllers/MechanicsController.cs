d64d5a669e2fdcc878644bd48717b5107e798082
MisaGrib <162205121+MisaGrib@users.noreply.github.com>
2026-05-29 12:23:23 +0300

    Modificat MechanicsController ca mecanicul sa aiba autorizare de lucru cu datele

--- Backend/Controllers/MechanicsController.cs
+++ Backend/Controllers/MechanicsController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 [ApiController]
 [Route("api/[controller]")]
 [Authorize(Roles = "Admin")]
-
 public class MechanicController : ControllerBase
 {
     private readonly IMechanicService _mechanicService;
@@ -23,72 +22,49 @@ public class MechanicController : ControllerBase
         return Ok(mechanics);
     }
 
-     [HttpGet("{id}")]
+    [HttpGet("{id}")]
+    [Authorize(Roles = "Admin,Mecanic")]
     public async Task<IActionResult> GetMechanicById(int id)
     {
         var mechanic = await _mechanicService.GetMechanicByIdAsync(id);
-
         if (mechanic == null)
-        {
             return NotFound($"Nu pot fi găsite datele pentru mecanicul cu id-ul: {id}");
-        }
-
         return Ok(mechanic);
     }
 
     [HttpGet("email/{email}")]
+    [Authorize(Roles = "Admin,Mecanic")]
     public async Task<IActionResult> GetMechanicByEmail(string email)
     {
         var mechanic = await _mechanicService.GetMechanicByEmailAsync(email);
-
         if (mechanic == null)
-        {
             return NotFound($"Nu pot fi găsite datele pentru mecanicul cu email-ul: {email}");
-        }
-
         return Ok(mechanic);
     }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] Mechanic newMechanic)
     {
-        if(newMechanic == null)
-        {
+        if (newMechanic == null)
             return BadRequest("Datele nu pot fi nule.");
-        }
-
         var existingMechanic = await _mechanicService.GetMechanicByEmailAsync(newMechanic.Email);
         if (existingMechanic != null)
-        {
             return Conflict($"Există deja un mecanic cu email-ul {newMechanic.Email}.");
-        }
-
         var createdMechanic = await _mechanicService.CreateMechanicAsync(newMechanic);
-
-        return CreatedAtAction(nameof(GetMechanicById), new{id = createdMechanic.Id}, createdMechanic);
+        return CreatedAtAction(nameof(GetMechanicById), new { id = createdMechanic.Id }, createdMechanic);
     }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(int id, [FromBody] Mechanic updatedMechanic)
     {
-      if(updatedMechanic == null)
-        {
+        if (updatedMechanic == null)
             return BadRequest("Datele nu pot fi nule.");
-        }
-
         var existingMechanic = await _mechanicService.GetMechanicByEmailAsync(updatedMechanic.Email);
         if (existingMechanic != null && existingMechanic.Id != id)
-        {
             return Conflict($"Email-ul {updatedMechanic.Email} este deja folosit de un alt mecanic.");
-        }
-
-        var isSucces = await _mechanicService.UpdateMechanicAsync(id, updatedMechanic);
-
-        if (!isSucces)
-        {
-            return NotFound($"Mecanicul nu a fost găsit");
-        }
-
+        var isSuccess = await _mechanicService.UpdateMechanicAsync(id, updatedMechanic);
+        if (!isSuccess)
+            return NotFound("Mecanicul nu a fost găsit");
         return NoContent();
     }
 
@@ -96,12 +72,8 @@ public class MechanicController : ControllerBase
     public async Task<IActionResult> Delete(int id)
     {
         var isDeleted = await _mechanicService.DeleteMechanicAsync(id);
-
         if (!isDeleted)
-        {
-            return NotFound($"Mecanicul nu a fost găsit");
-        }
-
+            return NotFound("Mecanicul nu a fost găsit");
         return NoContent();
     }
 }
